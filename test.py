f371e585b6e40712106cbcec81bc996f09295611
Astragor <astragor@DESKTOP-O58667I.play.pl>
2025-05-09 08:10:21 +0200

    added-file

---
+++ test.py
@@ -0,0 +1,22 @@
+class Car():  
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+
+    def drive(self):
+       print(f"I'm driving with the speed of {self.speed} kmph")
+
+    def printsome(self):
+        print(f"some bullshit")
+    pass
+asd = Car("Mercedes", 149)
+asd.drive() 
+asd.prinitsome()
+
+
+
+
+
+
+
+
